e9bd56943c79f4d9c3d5baf38d01f1c78a89f167
Oxmix <oxmix@me.com>
2025-02-10 22:39:10 +0300

    bump to v1.1.0 bump electron added search cmd/ctrl+f added links intercept to new window

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const {app, Menu, screen, BrowserWindow, session, ipcMain, dialog} = require('electron');
+const {app, Menu, screen, BrowserWindow, session, ipcMain, dialog, shell} = require('electron');
 const fs = require('fs');
 const path = require('path');
 
@@ -41,6 +41,77 @@ function runGpt(data, alert) {
     proxyRules: `https://${proxy.host}:${proxy.port}`
   }).then(() => {
     win.loadURL('https://chatgpt.com');
+
+    // custom search cmd/ctrl+f
+    ipcMain.on('start-search', (event, text, direction) => {
+      if (!win || !text.length) {
+        return
+      }
+      let opts = {}
+      if (direction === 'forward') {
+        opts.forward = direction === 'forward'
+      }
+      win.webContents.findInPage(text, opts);
+    });
+
+    ipcMain.on('stop-search', () => {
+      if (win) {
+        win.webContents.stopFindInPage('clearSelection');
+      }
+    });
+
+    win.webContents.on('did-finish-load', () => {
+      win.webContents.executeJavaScript(`
+      window.addEventListener("keydown", e => {
+        if ((e.ctrlKey || e.metaKey) && e.key === "f") {
+          e.preventDefault();
+          let searchBar = document.getElementById("customSearchBar");
+          if (searchBar) {
+            return
+          }
+          searchBar = document.createElement('div');
+          searchBar.innerHTML = \`
+            <input type="text" id="searchInput" placeholder="Search..." style="width: 200px; padding: 5px; color: #333 !important;">
+            <button id="nextButton" style="padding: 5px 10px; background: rgba(255, 255, 255, 0.5); margin-left: 8px; color: #333;">></button>
+            <button id="previousButton" style="padding: 5px 10px; background: rgba(255, 255, 255, 0.5); margin-left: 8px; color: #333;"><</button>
+            <button id="closeSearch" style="padding: 5px 10px; background: rgba(255, 255, 255, 0.5); margin-left: 8px; color: #333;">×</button>
+            <div style="color: #333;padding: 6px 0 0;">Found: <span id="found-in-page">–</span></div>
+          \`;
+          searchBar.style.cssText = 'position: fixed;top: 26px;background: rgba(255, 255, 255, .8);border-radius: 12px;padding: 16px 18px;z-index: 1000000;left: 0;right: 0;width: 365px;margin: auto;';
+          searchBar.id = 'customSearchBar';
+          document.body.appendChild(searchBar);
+          const input = document.getElementById('searchInput');
+          input.focus();
+          input.addEventListener('keydown', ev => {
+            if (13 == ev.keyCode) {
+                window.electron.startSearch(input.value);
+            }
+          });
+          document.getElementById('nextButton').addEventListener('click', () => {
+            window.electron.startSearch(input.value, 'forward');
+          });
+          document.getElementById('previousButton').addEventListener('click', () => {
+            window.electron.startSearch(input.value, 'back');
+          });
+          document.getElementById('closeSearch').addEventListener('click', () => {
+            searchBar.remove();
+            window.electron.stopSearch();
+          });
+        }
+      });`);
+    });
+
+    win.webContents.on('found-in-page', (event, result) => {
+      win.webContents.executeJavaScript(`
+        document.getElementById('found-in-page').innerText = ` + (result.matches - 1) + `;
+      `)
+    });
+  });
+
+  // handler links
+  win.webContents.setWindowOpenHandler(({url}) => {
+    shell.openExternal(url).then();
+    return {action: 'deny'};
   });
 
   // check auth proxy
@@ -59,7 +130,7 @@ function runGpt(data, alert) {
 }
 
 async function createWindow() {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  const {width, height} = screen.getPrimaryDisplay().workAreaSize;
 
   win = new BrowserWindow({
     width: Math.floor(width * 0.6),
@@ -86,7 +157,7 @@ async function createWindow() {
     }
   }
 
-  win.loadFile(path.join(__dirname, 'index.html'))
+  await win.loadFile(path.join(__dirname, 'index.html'))
   win.webContents.send('load-data', {
     proxy: '',
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.162 Safari/537.36'
@@ -119,7 +190,7 @@ app.whenReady().then(() => {
         {
           label: 'Delete all user data (uninstall)',
           click: () => {
-            fs.rm(userDataPath, { recursive: true, force: true }, (err) => {
+            fs.rm(userDataPath, {recursive: true, force: true}, (err) => {
               if (err) {
                 console.error('Err user data folder:', err);
               } else {
@@ -128,35 +199,35 @@ app.whenReady().then(() => {
             });
           }
         },
-        { role: 'quit' }
+        {role: 'quit'}
       ]
     },
     {
       label: 'Edit',
       submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        { role: 'pasteAndMatchStyle' },
-        { role: 'delete' },
-        { role: 'selectAll' }
+        {role: 'undo'},
+        {role: 'redo'},
+        {type: 'separator'},
+        {role: 'cut'},
+        {role: 'copy'},
+        {role: 'paste'},
+        {role: 'pasteAndMatchStyle'},
+        {role: 'delete'},
+        {role: 'selectAll'}
       ]
     },
     {
       label: 'View',
       submenu: [
-        { role: 'reload' },
-        { role: 'forcereload' },
-        { role: 'toggledevtools' },
-        { type: 'separator' },
-        { role: 'resetzoom' },
-        { role: 'zoomin' },
-        { role: 'zoomout' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
+        {role: 'reload'},
+        {role: 'forcereload'},
+        {role: 'toggledevtools'},
+        {type: 'separator'},
+        {role: 'resetzoom'},
+        {role: 'zoomin'},
+        {role: 'zoomout'},
+        {type: 'separator'},
+        {role: 'togglefullscreen'}
       ]
     },
   ];
--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,7 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   saveData: (data) => ipcRenderer.send('save-data', data),
   onLoadData: (callback) => ipcRenderer.on('load-data', (event, data) => callback(data)),
+  startSearch: (text) => ipcRenderer.send('start-search', text),
 });
